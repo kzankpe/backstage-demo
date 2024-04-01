9c5c543b2fe66a08e4a982e76b384acdb46dc00e
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-04-01 20:21:58 +0200

    feat: #1 add code to deploy infra (#4)
    * feat: #1 add code to deploy infra
    
    * fix: #1 correct error from PR
    
    * fix: #1 remove required version
    
    * fix: #1 update resource configuration
    
    * update folder name

---
+++ cloud-infra/naming.tf
@@ -0,0 +1,39 @@
+resource "azurecaf_name" "rg_name" {
+  name          = var.loc_abbreviation
+  resource_type = "azurerm_resource_group"
+  prefixes      = [var.prefix]
+  separator     = ""
+  suffixes      = [var.env_type]
+}
+
+resource "azurecaf_name" "acr_name" {
+  name          = var.loc_abbreviation
+  resource_type = "azurerm_container_registry"
+  prefixes      = [var.prefix]
+  separator     = ""
+  suffixes      = [var.env_type]
+}
+
+resource "azurecaf_name" "log_name" {
+  name          = var.loc_abbreviation
+  resource_type = "azurerm_log_analytics_workspace"
+  prefixes      = [var.prefix]
+  separator     = ""
+  suffixes      = [var.env_type]
+}
+
+resource "azurecaf_name" "app_env_name" {
+  name          = var.loc_abbreviation
+  resource_type = "azurerm_container_app_environment"
+  prefixes      = [var.prefix]
+  separator     = ""
+  suffixes      = [var.env_type]
+}
+
+resource "azurecaf_name" "container_app_name" {
+  name          = var.loc_abbreviation
+  resource_type = "azurerm_container_app"
+  prefixes      = [var.prefix]
+  separator     = ""
+  suffixes      = [var.env_type]
+}
--- cloud-infra/providers.tf
+++ cloud-infra/providers.tf
@@ -1,17 +1,23 @@
 terraform {
-    required_version = "=1.7.5-1"
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.97.1"
+      source  = "hashicorp/azurerm"
+      version = "=3.97.1"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "=1.2.28"
     }
   }
   backend "azurerm" {
-    
+
   }
 }
 
 provider "azurerm" {
   features {
   }
+  skip_provider_registration = true
+  use_msi                    = true
+
 }
